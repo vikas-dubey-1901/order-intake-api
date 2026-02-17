4fc8d640e0e958df4a2080fa908acc2b37a01d48
Vikas Dubey <50423857+vikas-dubey-1901@users.noreply.github.com>
2026-02-17 11:52:03 +0530

    Security Config and Web config added
    Basic as of now , will enhance it later.

--- src/main/java/com/processor/orderprocessing/config/WebConfig.java
+++ src/main/java/com/processor/orderprocessing/config/WebConfig.java
@@ -1,4 +1,29 @@
 package com.processor.orderprocessing.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Instant;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")   // restrict in production
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+
+        registry.addConverter(String.class, Instant.class,
+                source -> Instant.parse(source)
+        );
+    }
 }
